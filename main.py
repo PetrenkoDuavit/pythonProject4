b90217885ef9a0d170ca2c6d772e61564ba74cd0
Anton898 <81709784+IvanovAnto@users.noreply.github.com>
2024-10-07 19:49:47 +0300

    HW 4.2

--- main.py
+++ main.py
@@ -2,17 +2,33 @@
 
 # homeworck 4.1
 
-numbersList = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
-newNumbersList = []
-listSpaceCounter = 0
+# numbersList = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
+# newNumbersList = []
+# listSpaceCounter = 0
+# lengthOfList = len(numbersList)
+# i = 0
+#
+# while i < lengthOfList: # цикл длится пока не закончится список
+#     if numbersList[i] == 0: # если ячейка = 0 добавляю 0 в конец нового списка
+#         newNumbersList.append(0)
+#     else:
+#         newNumbersList.insert(listSpaceCounter, numbersList[i]) # в ином случае добавляю в новый список по адресу listSpaceCounter
+#         listSpaceCounter += 1
+#     i += 1
+# print(newNumbersList)
+
+# homeWorck 4.2
+
+numbersList = [0, 1, 7, 2, 4, 8]
+multiplicationNumbers = 0
 lengthOfList = len(numbersList)
+a = 0
 i = 0
 
-while i < lengthOfList: # цикл длится пока не закончится список
-    if numbersList[i] == 0: # если ячейка = 0 добавляю 0 в конец нового списка
-        newNumbersList.append(0)
-    else:
-        newNumbersList.insert(listSpaceCounter, numbersList[i]) # в ином случае добавляю в новый список по адресу listSpaceCounter
-        listSpaceCounter += 1
+while i < lengthOfList:
+    if i % 2 == 0:
+        multiplicationNumbers += numbersList[i]
     i += 1
-print(newNumbersList)
+print(multiplicationNumbers * numbersList[-1])
+
+
